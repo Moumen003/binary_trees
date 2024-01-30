9862168384c976dca57c3327a36ca506ea14c4d2
Moumen003 <moumenibrahim219@yahoo.com>
2024-01-30 16:50:48 +0200

    dfs

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,23 @@
+#include "binary_trees.h"
+
+
+/**
+* binary_tree_node - fn name
+* @parent:the parent node
+* @value: the value of the parent
+*
+* Return: the binary_tree_t type
+*/
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_s *newnode;
+
+	newnode = malloc(sizeof(binary_tree_s));
+	newnode->n = value;
+	newnode->parent = parent;
+	newnode->left = NULL;
+	newnode->right = NULL;
+	return (newnode);
+
+}
